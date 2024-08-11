10badd85f7137a0263ce0a1d29216af10613cf65
Amanbanti <amanbanti2011@gmail.com>
2024-08-12 00:27:27 +0300

    main page commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,67 @@
-import './layout.scss'
-import Navbar from './components/Navbar/Navbar.jsx';
+import HomePage from "./routes/homePage/homePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ListPage from "./routes/listPage/listPage";
+import { Layout, RequireAuth } from "./routes/layout/layout";
+import SinglePage from "./routes/singlePage/singlePage";
+// import ProfilePage from "./routes/profilePage/profilePage";
+// import Login from "./routes/login/login";
+// import Register from "./routes/register/register";
+// import ProfileUpdatePage from "./routes/profileUpdatePage/profileUpdatePage";
+// import NewPostPage from "./routes/newPostPage/newPostPage";
+// import { listPageLoader, profilePageLoader, singlePageLoader } from "./lib/loaders";
 
 function App() {
-  return (
-    <div className="layout">
-        <Navbar/>
-    </div>
-  ) 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+          
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+
+        // {
+        //   path: "/login",
+        //   element: <Login />,
+        // },
+        // {
+        //   path: "/register",
+        //   element: <Register />,
+        // },
+      ],
+    },
+    // {
+    //   path: "/",
+    //   element: <RequireAuth />,
+    //   children: [
+    //     {
+    //       path: "/profile",
+    //       element: <ProfilePage />,
+    //       loader: profilePageLoader
+    //     },
+    //     {
+    //       path: "/profile/update",
+    //       element: <ProfileUpdatePage />,
+    //     },
+    //     {
+    //       path: "/add",
+    //       element: <NewPostPage />,
+    //     },
+    //   ],
+    // },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
