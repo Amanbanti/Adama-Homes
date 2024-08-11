834e4753f58e99302f3745ba753712963d8d396a
Amanbanti <amanbanti2011@gmail.com>
2024-08-12 00:26:18 +0300

    Map commit

---
+++ src/components/map/Map.jsx
@@ -0,0 +1,44 @@
+// import { MapContainer, TileLayer } from "react-leaflet";
+// import "./map.scss";
+// // import "leaflet/dist/leaflet.css";
+// // import Pin from "../pin/Pin";
+
+// function Map({ items }) {
+//   return (
+//     <MapContainer
+//       center={
+//         items.length === 1
+//           ? [items[0].latitude, items[0].longitude]
+//           : [52.4797, -1.90269]
+//       }
+//       zoom={7}
+//       scrollWheelZoom={false}
+//       className="map"
+//     >
+//       <TileLayer
+//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//       />
+//       {items.map((item) => (
+//         <Pin item={item} key={item.id} />
+//       ))}
+//     </MapContainer>
+//   );
+// }
+
+// export default Map;
+
+
+
+import React from 'react';
+
+function Map() {
+  return (
+    <div>
+      <h1>Map</h1>
+      
+    </div>
+  );
+}
+
+export default Map;
