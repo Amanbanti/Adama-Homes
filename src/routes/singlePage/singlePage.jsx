b9c64cb0328fdbbdd47cf67a614c9631ea0c4d52
Amanbanti <amanbanti2011@gmail.com>
2024-08-12 00:12:08 +0300

    singlepage commit

---
+++ src/routes/singlePage/singlePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function singlePage() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a simple React functional component.</p>
+    </div>
+  );
+}
+
+export default singlePage;
