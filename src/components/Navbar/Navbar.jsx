9d91e880f5474105544a0561ba0c35e7f6f40288
Amanbanti <amanbanti2011@gmail.com>
2024-08-15 14:50:16 +0300

    final commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,35 +1,31 @@
- import React from 'react'
- import './navbar.scss'
- import { useState } from 'react';
- import { Link } from 'react-router-dom';
+import { useState } from "react";
+import "./navbar.scss";
+import { Link } from "react-router-dom";
 
- const Navbar = () => {
-  const [open ,setOpen] = useState(false);
+function Navbar() {
+  const [open, setOpen] = useState(false);
+
   const user = true;
-   return (  
+  return (
     <nav>
-        <div className='left'>
-          <a href='/' className='logo'>
-             <img src="/BihouseDoor5_x2.svg" alt='Adama-Homes-logo'/>
-             <span>Adama Homes</span>
-          </a>
-
-          <a href='/'>Home</a>
-          <a href='/'>About</a>
-          <a href='/'>Contact</a>
-          <a href='/'>Agents</a>
-
-
-        </div>
-
-        <div className='right'>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="" />
+          <span>LamaEstate</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
         {user ? (
           <div className="user">
             <img
               src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
               alt=""
             />
-            <span>Aman Banti</span>
+            <span>John Doe</span>
             <Link to="/profile" className="profile">
               <div className="notification">3</div>
               <span>Profile</span>
@@ -43,24 +39,24 @@
             </a>
           </>
         )}
-
-              <div className={open ? "menu active":"menu"}>
-                  <a href='/'>Home</a>
-                  <a href='/'>About</a>
-                  <a href='/'>Contact</a>
-                  <a href='/'>Agents</a>
-                  <a href='/'>Sign in </a>
-                  <a href='/'>Sign up</a>
-
-              </div>
-
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
         </div>
-
-
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
+      </div>
     </nav>
-    
-   )
- }
- 
- export default Navbar
- 
+  );
+}
+
+export default Navbar;
